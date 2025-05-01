dd6001c271f543349680bbb075afc534a3cb4349
jaghali <m4gaming37@gmail.com>
2025-05-01 14:48:34 +0100

    salouha9ballekhers

--- frontend/src/components/AIAssistantCard.js
+++ frontend/src/components/AIAssistantCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { motion } from "framer-motion";
-import { useParams, useNavigate } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 const AIAssistantCard = () => {
   const navigate = useNavigate();
